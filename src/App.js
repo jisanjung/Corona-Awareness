50e65933c402a639cc9b8fad0ef8d78614887ae9
Jisan Jung <jjjung13@gmail.com>
2020-10-28 22:14:58 -0400

    about section finished

--- src/App.js
+++ src/App.js
@@ -9,25 +9,36 @@ import Tag from './components/Tag';
 
 export class App extends Component {
   render() {
+
+    const dark = "#35404e";
+    const gray = "#8a8eaa";
+    const darkBlue = "#294e7d";
+    const purple = "#706ad4";
+
     return (
       <div className="App">
           <Header/>
-        <div className="home">
+        <div className="home section">
           <div className="container"> 
-            <Title color="#294e7d" size="1.125rem" content="Stay Home, Stay Safe"/>
-            <Title color="#706ad4" size="2.25rem" content="Protect Yourself From Coronavirus (COVID-19)"/>
-            <Text color="#8a8eaa" content="Learn how to help flatten the curve of a deadly virus."/>
+            <Title color={darkBlue} size="1.125rem" content="Stay Home, Stay Safe"/>
+            <Title color={purple} size="2.25rem" content="Protect Yourself From Coronavirus (COVID-19)"/>
+            <Text color={gray} content="Learn how to help flatten the curve of a deadly virus."/>
             <Button type="primary" dest="#Prevention" content="Prevention"/>
             <Button type="light" dest="/" content="Tracker"/>
           </div>
         </div>
-        <div className="about">
+        <div className="about section">
           <div className="container flex">
             <div className="img-wrap">
               <img src={about} alt="About"/>
             </div>
             <div className="info">
               <Tag content="About COVID-19"/>
+              <Title color={dark} size="2rem" content="What is novel coronavirus?"/>
+              <Text color={gray} content="More than 78,191 people have contracted the virus in China. Health authorities have identified many other people with COVID-19 around the world, including in the United
+              States. On January 31, 2020, the virus pass from one person to another in the U.S.
+              Coronaviruses are a type of virus. There are many different kinds."/>
+              <Button type="primary" dest="#Symptoms" content="Symptoms"/>
             </div>
           </div>
         </div>

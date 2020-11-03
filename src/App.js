403487d114b548f46a84e04c6e73837f4e582685
Jisan Jung <jjjung13@gmail.com>
2020-11-02 22:15:45 -0500

    symptoms page completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import "./sass/main.scss";
 import Button from './components/Button';
 import about from "./images/about.png";
 import Tag from './components/Tag';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 
 export class App extends Component {
   render() {
@@ -39,6 +41,27 @@ export class App extends Component {
               States. On January 31, 2020, the virus pass from one person to another in the U.S.
               Coronaviruses are a type of virus. There are many different kinds."/>
               <Button type="primary" dest="#Symptoms" content="Symptoms"/>
+            </div>
+          </div>
+        </div>
+        <div className="symptoms section">
+          <div className="container flex">
+            <div className="info">
+              <Tag content="COVID-19 Symptoms"/>
+              <Title color={dark} size="2rem" content="What are the symptoms?"/>
+              <Text color={gray} content="It's important to know what symptoms are associated. Please seek medical attention if you are experiencing:"/>
+              <ul>
+                <li className="flex direction-row align-center"><FontAwesomeIcon icon={faCheckCircle}/><Text color={gray} content="Sore Throat"/></li>
+                <li className="flex direction-row align-center"><FontAwesomeIcon icon={faCheckCircle}/><Text color={gray} content="Fever"/></li>
+                <li className="flex direction-row align-center"><FontAwesomeIcon icon={faCheckCircle}/><Text color={gray} content="Runny nose"/></li>
+                <li className="flex direction-row align-center"><FontAwesomeIcon icon={faCheckCircle}/><Text color={gray} content="Headache"/></li>
+                <li className="flex direction-row align-center"><FontAwesomeIcon icon={faCheckCircle}/><Text color={gray} content="Fatigue"/></li>
+                <li className="flex direction-row align-center"><FontAwesomeIcon icon={faCheckCircle}/><Text color={gray} content="Dry Cough"/></li>
+              </ul>
+              <Button type="primary" dest="#Prevention" content="Prevention"/>
+            </div>
+            <div>
+
             </div>
           </div>
         </div>

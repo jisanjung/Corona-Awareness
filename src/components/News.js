d07f5c08a9226899210c47cdd388349b7c1e68af
Jisan Jung <jjjung13@gmail.com>
2020-11-19 23:22:04 -0500

    url

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 const API_KEY = process.env.REACT_APP_API_KEY;
+let url = `https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=${API_KEY}`;
 
 export class News extends Component {
 

d8a85babf8ba11bb653ea0f506d0fa70d324c489
Jisan Jung <jjjung13@gmail.com>
2020-12-02 20:12:53 -0500

    article card styling

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import Header from './Header';
+import Title from './Title';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 let url = `https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=${API_KEY}`;
@@ -32,21 +35,31 @@ export class News extends Component {
     }
 
     render() {
+
+        const rightArrow = <FontAwesomeIcon icon={faArrowRight}/>
+
         return (
             <div className="news">
                 <Header/>
-                <h1>Latest News</h1>
-                <ul className="articles">
-                    {this.state.news.map((article, i) => {
-                        return (
-                            <li key={i} className="article">
-                                <img src={article.urlToImage} alt={i}/>
-                                <h3>{article.title}</h3>
-                                <a href={article.url}>Read More</a>
-                            </li>
-                        )
-                    })}
-                </ul>
+                <div className="container">
+                    <Title size="1.5rem" content="Latest News"/>
+                    <ul className="articles">
+                        {this.state.news.map((article, i) => {
+                            return (
+                                <li key={i} className="article">
+                                    <div className="img-wrap">
+                                        <img src={article.urlToImage} alt={i}/>
+                                    </div>
+                                    <div className="info">
+                                        <h3>{article.title}</h3>
+                                        <a className="text-dark flex align-center" href={article.url}>Read More<span>{rightArrow}</span></a>
+                                        <span className="date-posted block">{article.publishedAt}</span>
+                                    </div>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
             </div>
         )
     }

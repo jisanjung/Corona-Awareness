0c9e4a47389a0ac4852d394f9abd7d10b16a4437
Jisan Jung <jjjung13@gmail.com>
2021-01-01 15:18:48 -0500

    styling daily-numbers data

--- src/components/DailyNumbers.js
+++ src/components/DailyNumbers.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import Loading from "./Loading.js";
+import moment from "moment";
 
 const DailyNumbers = () => {
 
@@ -8,7 +9,6 @@ const DailyNumbers = () => {
     const [cases, setCases] = useState("");
     const [deaths, setDeaths] = useState("");
     const [recovered, setRecovered] = useState("");
-    
 
     useEffect(() => {
         axios.get("https://corona.lmao.ninja/v3/covid-19/all")
@@ -20,17 +20,30 @@ const DailyNumbers = () => {
         });
     }, []);
 
+    // converts unix time stamp to YYYYMMDD h:mm:ss a
+    const unixToDate = unix => {
+        let formatted = moment(unix).format("YYYYMMDD, h:mm:ss a");
+        return formatted; // ex: 20200824 11:59:00 pm
+    }
+
+    let date = unixToDate(updated);
+    let timeUpdated = moment(date, "YYYYMMDD, h:mm:ss a").fromNow(); // ex: 10 minutes ago
+
     return (
+        
         <div>
-            <span className="updated">{updated ? `last updated: ${updated}` : <Loading/>}</span>
-            <div className="cases">
-                {cases ? `Cases: ${cases}` : <Loading/>}
+            <span className="updated text-right block">{updated ? `last updated: ${timeUpdated}` : <Loading/>}</span>
+            <div className="cases daily-item">
+                <span>Global Cases: </span>
+                {cases ? cases : <Loading/>}
             </div>
-            <div className="deaths">
-                {deaths ? `Deaths: ${deaths}` : <Loading/>}
+            <div className="deaths daily-item">
+                <span>Deaths: </span>
+                {deaths ? deaths : <Loading/>}
             </div>
-            <div className="recovered">
-                {recovered ? `Recovered: ${recovered}` : <Loading/>}
+            <div className="recovered daily-item">
+                <span>Recovered: </span>
+                {recovered ? recovered : <Loading/>}
             </div>
         </div>
     )
